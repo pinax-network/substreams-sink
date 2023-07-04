75e15917219c117d0f58e027ad6ecca2c223169c
Charles Morin <charles.mor@outlook.fr>
2023-07-04 10:27:53 -0400

    Removed auto-restart option

--- src/commander.ts
+++ src/commander.ts
@@ -20,7 +20,6 @@ export interface RunOptions {
     cursorFile?: string;
     disableProductionMode?: boolean;
     verbose?: boolean;
-    autoRestart?: boolean;
     restartInactivitySeconds?: number;
 }
 
