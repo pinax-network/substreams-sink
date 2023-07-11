f72c3cc0b6e92d0ddbc3786f032adef31aa933ae
Denis Carriere <carriere.denis@gmail.com>
2023-07-11 11:52:43 -0400

    update example

--- example.ts
+++ example.ts
@@ -1,6 +1,5 @@
 import pkg from "./package.json" assert { type: "json" };
-import { commander, setup, prometheus } from "./index.js";
-import { listen } from "./src/http";
+import { commander, setup, prometheus, http } from "./index.js";
 
 // Setup CLI using Commander
 const program = commander.program(pkg);
@@ -22,7 +21,7 @@ command.action(async (options: commander.RunOptions) => {
   });
 
   // Setup HTTP server & Prometheus metrics
-  listen(options);
+  http.listen(options);
 
   // Start streaming
   emitter.start();
