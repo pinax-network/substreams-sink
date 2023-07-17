60a24ed73ce41fbdb47ae20282f3944c4000f044
Denis Carriere <carriere.denis@gmail.com>
2023-07-17 15:43:43 -0400

    add onRestartInactivitySeconds

---
+++ src/onRestartInactivitySeconds.ts
@@ -0,0 +1,17 @@
+import type { BlockEmitter } from "@substreams/node";
+import { logger } from "./logger.js";
+
+export function onRestartInactivitySeconds(emitter: BlockEmitter, restartInactivitySeconds: number) {
+    let lastUpdate = now();
+    emitter.on("cursor", () => {
+        const dif = Math.abs(now() - lastUpdate);
+        if (dif > restartInactivitySeconds) {
+            logger.error(`Restarting due to inactivity for ${dif} seconds`);
+            process.exit(1);
+        }
+    });
+}
+
+export function now() {
+    return Math.floor(new Date().getTime() / 1000); // in seconds
+}
