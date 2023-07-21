5b9667a9c3b90e2d4ed6623cc21da36085501feb
Charles Morin <charles.mor@outlook.fr>
2023-07-21 14:21:13 -0400

    Fixed --restart-inactivity-seconds

--- src/restartInactivitySeconds.ts
+++ src/restartInactivitySeconds.ts
@@ -1,15 +1,28 @@
 import type { BlockEmitter } from "@substreams/node";
+import { setTimeout } from "timers/promises";
 import { logger } from "./logger.js";
+
+const CHECK_INACTIVITY_INTERVAL = 1000;
 
 export function onRestartInactivitySeconds(emitter: BlockEmitter, restartInactivitySeconds: number) {
     let lastUpdate = now();
-    emitter.on("cursor", () => {
+    let isStarted = false;
+
+    async function checkInactivity() {
         if (now() - lastUpdate > restartInactivitySeconds) {
+            if (!isStarted) return;
             logger.error(`Restarting due to inactivity for ${restartInactivitySeconds} seconds`);
             process.exit(1);
         }
+        await setTimeout(CHECK_INACTIVITY_INTERVAL);
+        checkInactivity();
+    }
+
+    emitter.on("cursor", () => {
+        isStarted = true;
         lastUpdate = now();
     });
+    checkInactivity();
 }
 
 export function now() {
