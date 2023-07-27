b63dd613b8eee8f07b86353664d801123ef62a6a
Charles Morin <charles.mor@outlook.fr>
2023-07-27 12:01:03 -0400

    Regrouped cursor exports in one index file

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 export * as commander from "./src/commander.js";
 export * as prometheus from "./src/prometheus.js";
-export * as fileCursor from "./src/cursor/fileCursor.js";
-export * as httpCursor from "./src/cursor/httpCursor.js";
+export * from "./src/cursor/index.js";
 export * as config from "./src/config.js";
 export * as http from "./src/http.js";
 export { logger } from "./src/logger.js";
---
+++ src/cursor/index.ts
@@ -0,0 +1,2 @@
+export * as fileCursor from "./fileCursor.js";
+export * as httpCursor from "./httpCursor.js";
