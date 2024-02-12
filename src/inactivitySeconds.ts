d38d0a8d8f473ce792371653ac1fa089c4aa1dcd
skcdev <solinscription@gmail.com>
2024-02-13 01:05:05 +1300

    track progress in onInactivitySeconds

--- src/inactivitySeconds.ts
+++ src/inactivitySeconds.ts
@@ -4,30 +4,50 @@ import { logger } from "./logger.js";
 
 const CHECK_INACTIVITY_INTERVAL = 1000;
 
-export function onInactivitySeconds(emitter: BlockEmitter, inactivitySeconds: number, hasStopBlock: boolean) {
-    let lastUpdate = now();
-    let isFinished = false;
+export function onInactivitySeconds(
+	emitter: BlockEmitter,
+	inactivitySeconds: number,
+	hasStopBlock: boolean
+) {
+	let lastUpdate = now();
+	let isFinished = false;
+	let lastTotalBytesRead = 0n;
+	let currentTotalBytesRead = 0n;
 
-    async function checkInactivity() {
-        if (now() - lastUpdate > inactivitySeconds) {
-            logger.error(`Process will exit due to inactivity for ${inactivitySeconds} seconds`);
-            process.exit(1); // force quit
-        }
-        if (isFinished) return; // exit out of the loop
-        await setTimeout(CHECK_INACTIVITY_INTERVAL);
-        checkInactivity();
-    }
+	async function checkInactivity() {
+		if (currentTotalBytesRead > lastTotalBytesRead) {
+			lastUpdate = now();
+			lastTotalBytesRead = currentTotalBytesRead;
+		}
 
-    // Check for inactivity after starting
-    emitter.on("anyMessage", (message, cursor, clock) => {
-        lastUpdate = now();
-        if (hasStopBlock && clock.number >= emitter.request.stopBlockNum - 1n) {
-            isFinished = true;
-        }
-    });
-    checkInactivity();
+		if (now() - lastUpdate > inactivitySeconds) {
+			logger.error(
+				`Process will exit due to inactivity for ${inactivitySeconds} seconds`
+			);
+			process.exit(1); // force quit
+		}
+		if (isFinished) return; // exit out of the loop
+		await setTimeout(CHECK_INACTIVITY_INTERVAL);
+		checkInactivity();
+	}
+
+	// Check for inactivity after starting
+	emitter.on("clock", (clock) => {
+		lastUpdate = now();
+		if (hasStopBlock && clock.number >= emitter.request.stopBlockNum - 1n) {
+			isFinished = true;
+		}
+	});
+
+	emitter.on("progress", (progress) => {
+		if (progress.processedBytes) {
+			currentTotalBytesRead = progress.processedBytes.totalBytesRead;
+		}
+	});
+
+	checkInactivity();
 }
 
 export function now() {
-    return Math.floor(new Date().getTime() / 1000); // in seconds
+	return Math.floor(new Date().getTime() / 1000); // in seconds
 }
